999430caf883ae6c53c90181e42a41332d746b78
Mayank Tatwade <mayanktatwadeiitb@gmail.com>
2026-06-22 23:43:36 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int ans=0;
+
+    void helper(string &s,int l, int r){
+        while(l>=0 && r<s.size() && s[l]==s[r]){
+            ans++;l--;r++;
+        }
+        return;
+
+    }
+    int countSubstrings(string s) {
+        for(int i=0; i<s.size(); i++){
+            helper(s,i,i);
+            helper(s,i,i+1);
+        }
+        return ans;
+    }
+};
