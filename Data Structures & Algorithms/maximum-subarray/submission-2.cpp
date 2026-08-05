557f2f37809e8ea7828a594a5a2db47f9a06f4fa
Mayank Tatwade <mayanktatwadeiitb@gmail.com>
2026-08-06 00:07:21 +0530

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1){return nums[0];}
+        int r = 0;
+        int total = 0; int ans = INT_MIN;
+        
+        while(r<n){
+            total += nums[r++];
+            ans = max(total,ans);
+            if(total<=0){total = 0;}
+        }
+        return ans;
+    }
+};
