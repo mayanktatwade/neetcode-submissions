088d49cbe1d4f50b67dc8bfb83b64edcb16803b0
Mayank Tatwade <mayanktatwadeiitb@gmail.com>
2026-08-06 22:58:50 +0530

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+
+        int minfuel = INT_MAX; int fuel=0; int idx=0;
+
+        for(int i=0; i<n; i++){
+            fuel+=(gas[i]-cost[i]);
+            if(fuel<minfuel){minfuel = fuel; idx = i;}
+        }
+        if(fuel<0){return -1;}
+        if(idx+1<n){return idx+1;}
+        return 0;
+    }
+};
